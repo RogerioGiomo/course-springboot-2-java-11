d39b492d6dd0d9178d520f81153a9eab2f18b506
wroger <wgiomo@gmail.com>
2020-05-04 13:26:58 -0300

    Produto Parte 2

--- src/main/java/com/wrgsistemas/course/Config/TestConfig.java
+++ src/main/java/com/wrgsistemas/course/Config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.wrgsistemas.course.entities.Category;
 import com.wrgsistemas.course.entities.Order;
+import com.wrgsistemas.course.entities.OrderItem;
 import com.wrgsistemas.course.entities.Product;
 import com.wrgsistemas.course.entities.User;
 import com.wrgsistemas.course.entities.enums.OrderStatus;
 import com.wrgsistemas.course.repositories.CategoryRepository;
+import com.wrgsistemas.course.repositories.OrderItemRepository;
 import com.wrgsistemas.course.repositories.OrderRepository;
 import com.wrgsistemas.course.repositories.ProductRepository;
 import com.wrgsistemas.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -68,6 +73,13 @@ public class TestConfig implements CommandLineRunner {
 		p5.getCategories().add(cat2);
 
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 	}
 	
 }
