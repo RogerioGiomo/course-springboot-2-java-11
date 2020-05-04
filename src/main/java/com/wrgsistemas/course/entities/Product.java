ba52eb0868f06522968d3eb2277a5260f63f2fd0
wroger <wgiomo@gmail.com>
2020-05-04 13:14:29 -0300

    Produto Parte 1

--- src/main/java/com/wrgsistemas/course/entities/Product.java
+++ src/main/java/com/wrgsistemas/course/entities/Product.java
@@ -8,6 +8,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -25,8 +29,11 @@ public class Product implements Serializable {
 	private Double price;
 	private String imgUrl;
 	
-	@Transient
-	private Set<Category> categories = new HashSet<>(); // garanti que nao seja nula mas sim vazia 
+	@ManyToMany
+	@JoinTable(name = "tb_product_category",
+			joinColumns = @JoinColumn(name = "product_id"),
+			inverseJoinColumns =  @JoinColumn(name = "category_id"))
+	private Set<Category> categories = new HashSet<>(); // garantir que nao seja nula mas sim vazia 
 	
 	public Product() {}
 
